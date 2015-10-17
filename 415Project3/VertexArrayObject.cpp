9d4c081fcad2c8ca02e0f5162ff892d80cddf459
sercril <august.montalbano@gmail.com>
2015-10-16 21:32:40 -0500

    making VAO

---
+++ 415Project3/VertexArrayObject.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <gmtl\gmtl.h>
+#include <gmtl\Matrix.h>
+
+
+
+#include "VertexArrayObject.h"
+
+using namespace std;
+
+VertexArrayObject(std::vector<GLfloat> * vertex_buffer, std::vector<GLfloat>* color_buffer, std::vector<GLfloat>* index_buffer);
+{
+
+}
